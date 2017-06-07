2b31e529cba120af70a7348caa67e39be0933c6f
Yoshiyuki Ito <thanks2music@gmail.com>
2017-06-07 16:48:32 +0900

    Fix

--- head.php
+++ head.php
@@ -1,5 +1,4 @@
 <?php if (get_option('other_options_ga')) { ?>
-<?php // GTM ?>
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -9,4 +8,4 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <!-- End Google Tag Manager -->
 <?php } ?>
 
-<?php echo get_option( 'other_options_headcode' ); ?>
+<?php echo get_option('other_options_headcode'); ?>
