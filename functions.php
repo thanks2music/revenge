5cf6ecad87db064bd4bade5abe933097e69b83c7
Yoshiyuki Ito <thanks2music@gmail.com>
2017-06-11 23:59:57 +0900

    [WIP] Add breadcrumb function

--- functions.php
+++ functions.php
@@ -75,3 +75,109 @@ EOF;
 
   return $contentData;
 }
+
+// パンくず
+if (!function_exists('breadcrumb')) {
+	function breadcrumb($divOption = array("id" => "breadcrumb", "class" => "breadcrumb inner wrap cf")){
+    global $post;
+    $str ='';
+    if(! get_option('side_options_pannavi')){
+      if (! is_home() && ! is_front_page() && ! is_admin() ){
+          $tagAttribute = '';
+          foreach($divOption as $attrName => $attrValue){
+              $tagAttribute .= sprintf(' %s="%s"', $attrName, $attrValue);
+          }
+          $str.= '<div'. $tagAttribute .'>';
+          $str.= '<ul>';
+          $str.= '<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><a href="'. home_url() .'/" itemprop="url"><i class="fa fa-home"></i><span itemprop="title"> HOME</span></a></li>';
+   
+          if (is_category()) {
+              $cat = get_queried_object();
+
+              if ($cat->parent != 0){
+                $ancestors = array_reverse(get_ancestors($cat->cat_ID, 'category' ));
+                foreach($ancestors as $ancestor){
+                  $str.='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><a href="'. get_category_link($ancestor) .'" itemprop="url"><span itemprop="title">'. get_cat_name($ancestor) .'</span></a></li>';
+                }
+              }
+
+              $str.='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><span itemprop="title">'. $cat -> name . '</span></li>';
+          } elseif (is_single()) {
+              $post_type = get_post_type();
+              $taxonomy_name = 'category';
+              if ($post_type === 'event') {
+                $taxonomy_name = 'event-category';
+              }
+
+              $categories = get_the_terms($post->ID, $taxonomy_name);
+              var_dump($categories);
+              $cat_len = count($categories);
+
+              if ($cat_len > 0) {
+                $cat = $categories[1];
+                // TODO 後で文字列以外の方法で
+                // for ($i= 0; $i < $cat_len; $i++) {
+                //   if ($cat[$i]->slug !== '') {
+                //   }
+                // }
+              } else {
+                $cat = $categories[0];
+              }
+
+              if ($cat->parent != 0) {
+                  $ancestors = array_reverse(get_ancestors( $cat -> cat_ID, $taxonomy_name));
+                  foreach($ancestors as $ancestor){
+                      $str.='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><a href="'. get_category_link($ancestor).'" itemprop="url"><span itemprop="title">'. get_cat_name($ancestor). '</span></a></li>';
+                  }
+              }
+
+              // Category
+              if ($post_type === 'post') {
+                $str.='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><a href="'. get_category_link($cat -> term_id). '" itemprop="url"><span itemprop="title">'. $cat-> cat_name . '</span></a></li>';
+                // Taxonomy
+              } else {
+                $str.='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><a href="'. get_term_link($cat->term_taxonomy_id, $taxonomy_name). '" itemprop="url"><span itemprop="title">'. $cat->name . '</span></a></li>';
+              }
+              $str.= '<li>'. $post -> post_title .'</li>';
+          } elseif(is_page()){
+              if($post -> post_parent != 0 ){
+                  $ancestors = array_reverse(get_post_ancestors( $post->ID ));
+                  foreach($ancestors as $ancestor){
+                      $str.='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><a href="'. get_permalink($ancestor).'" itemprop="url"><span itemprop="title">'. get_the_title($ancestor) .'</span></a></li>';
+                  }
+              }
+              $str.= '<li>'. $post -> post_title .'</li>';
+          } elseif(is_date()){
+        if( is_year() ){
+          $str.= '<li>' . get_the_time('Y') . '年</li>';
+        } else if( is_month() ){
+          $str.= '<li><a href="' . get_year_link(get_the_time('Y')) .'">' . get_the_time('Y') . '年</a></li>';
+          $str.= '<li>' . get_the_time('n') . '月</li>';
+        } else if( is_day() ){
+          $str.= '<li><a href="' . get_year_link(get_the_time('Y')) .'">' . get_the_time('Y') . '年</a></li>';
+          $str.= '<li><a href="' . get_month_link(get_the_time('Y'),get_the_time('m')) . '">' . get_the_time('n') . '月</a></li>';
+          $str.= '<li>' . get_the_time('j') . '日</li>';
+        }
+        if(is_year() && is_month() && is_day() ){
+          $str.= '<li>' . wp_title('', false) . '</li>';
+        }
+          } elseif(is_search()) {
+              $str.='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><span itemprop="title">「'. get_search_query() .'」で検索した結果</span></li>';
+          } elseif(is_author()){
+              $str .='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><span itemprop="title">投稿者 : '. get_the_author_meta('display_name', get_query_var('author')).'</span></li>';
+          } elseif(is_tag()){
+              $str.='<li itemscope itemtype="//data-vocabulary.org/Breadcrumb"><span itemprop="title">タグ : '. single_tag_title( '' , false ). '</span></li>';
+          } elseif(is_attachment()){
+              $str.= '<li><span itemprop="title">'. $post -> post_title .'</span></li>';
+          } elseif(is_404()){
+              $str.='<li>ページがみつかりません。</li>';
+          } else{
+              $str.='<li></li>';
+          }
+          $str.='</ul>';
+          $str.='</div>';
+      }
+  }
+    echo $str;
+	}
+}
