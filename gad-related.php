677737eeff200ce05e875d3d432f447dfb905f1e
Yoshi Ito <thanks2music@gmail.com>
2018-06-01 12:37:48 +0900

    広告ユニット毎にadsbygoogle.js読まないと動作しない時があるので復活

--- gad-related.php
+++ gad-related.php
@@ -2,6 +2,7 @@
   <div class="inbox">
     <h2 class="related-h h_ttl"><span class="gf">RELATED</span>こんな記事も読まれています</h2>
     <div class="related-post">
+      <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       <ins class="adsbygoogle"
            style="display:block"
            data-ad-format="autorelaxed"
