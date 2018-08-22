7b0973946d42104a1d0fa34f05ef48305a4b0444
Yoshi Ito <thanks2music@gmail.com>
2018-08-22 14:18:27 +0900

    Add favicon for AMP

--- storkre-child/single-amp.php
+++ storkre-child/single-amp.php
@@ -1,6 +1,5 @@
 <?php global $is_sp, $is_pc, $amp_flag, $dir, $url;
 
-
 if ($amp_flag) {
   $file_path = $dir['theme'] . '/dist/css/amp.css';
   $file_path = mb_substr($file_path, 25);
@@ -63,6 +62,7 @@ if ($amp_flag) {
     <script async custom-element="amp-lightbox-gallery" src="https://cdn.ampproject.org/v0/amp-lightbox-gallery-0.1.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Concert+One|Lato" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="icon" href="<?php echo $url['home']; ?>/wp-content/uploads/favicon_64.png">
     <script async custom-element="amp-youtube" src="https://cdn.ampproject.org/v0/amp-youtube-0.1.js"></script>
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
     <script async src="https://cdn.ampproject.org/v0.js"></script>
