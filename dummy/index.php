48a84eaa3697772412eaf458faf1a1581baf066f
Yoshi Ito <thanks2music@gmail.com>
2018-12-31 16:17:44 +0900

    Add google tag manager

--- dummy/index.php
+++ dummy/index.php
@@ -5,8 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1 ,viewport-fit=cover"/>
     <meta name="robots" content="noindex,nofollow">
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-T73N2NP');</script>
   </head>
   <body>
     <?php // This is Dummy Theme for Redirect ?>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T73N2NP"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
   </body>
 </html>
