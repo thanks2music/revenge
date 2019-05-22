9b9020c4f3aedd3f511224b597005ca6c22ed519
Yoshi Ito <thanks2music@gmail.com>
2019-05-22 20:26:36 +0900

    Add YDN tag for SP

--- storkre-child/header.php
+++ storkre-child/header.php
@@ -39,6 +39,12 @@
         enable_page_level_ads: true
       });
     </script>
+    <?php if ($is_sp) { // YDN Tag ?>
+      <script src="//cdn.gmossp-sp.jp/ads/loader.js?space_id=g908845" charset="utf-8" language="JavaScript"></script>
+      <script language='javascript'>
+          gmossp_forsp.ad('g908845');
+      </script>
+    <?php } ?>
   <?php } ?>
   </head>
   <?php
