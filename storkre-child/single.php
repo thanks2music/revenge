c2a8e379a67c72a69b18a47b4b17c7530cea74d0
Yoshi Ito <thanks2music@gmail.com>
2019-06-21 14:51:39 +0900

    Googleのレコメンド位置変更

--- storkre-child/single.php
+++ storkre-child/single.php
@@ -112,6 +112,16 @@
     </footer>
     <?php endif; ?>
 
+    <?php // Google Recommend ?>
+    <div class="ad__google-recommend">
+      <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-7307810455044245"
+        data-ad-slot="2987528412"
+        data-matched-content-rows-num="5" data-matched-content-columns-num="1"
+        data-matched-content-ui-type="image_sidebyside" data-ad-format="autorelaxed"></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+    </div>
     <?php if ($is_sp) { ?>
       <div class="ad__taxel__gmo-recommend">
         <div id="gmo_rw_12370" data-gmoad="rw"></div>
