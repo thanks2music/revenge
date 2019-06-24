3fe9bc72b154b9bb6c992dd75c18b9506f8bf963
Yoshi Ito <thanks2music@gmail.com>
2019-06-24 17:27:00 +0900

    Add author post list

--- storkre-child/author.php
+++ storkre-child/author.php
@@ -1,1 +1,23 @@
-<?php get_template_part('archive'); ?>
+<?php global $is_sp, $is_pc; ?>
+<?php get_header(); ?>
+<div id="content">
+  <div id="inner-content" class="wrap cf">
+    <main id="main" class="m-all t-all d-5of7 cf" role="main">
+      <div class="archivettl">
+        <?php
+          $archive_title = 'archive__title__author';
+          $author_name = get_the_author_meta('display_name', $author);
+        ?>
+        <h1 class="archive-title h2 <?php echo $archive_title; ?>">
+          <span class="archive__title__author-icon"><?php echo get_avatar($author); ?></span>
+          <p class="archive__title__author-text">「<?php echo $author_name; ?>」さんが書いた記事一覧</p>
+        </h1>
+      </div>
+
+      <?php get_template_part( 'parts_archive_simple' ); ?>
+    </main>
+    <?php get_sidebar(); ?>
+  </div>
+</div>
+<?php get_footer(); ?>
+
