2a432b621899b4adef1b5ccf2551fec603f297c7
Yoshi Ito <thanks2music@gmail.com>
2019-06-25 13:10:03 +0900

    自己紹介が入力されていない場合は表示しない

--- storkre-child/partials/author.php
+++ storkre-child/partials/author.php
@@ -6,22 +6,25 @@
   $author_name = get_the_author_meta('display_name', $author_id);
   $author_description = get_the_author_meta('description', $author_id);
   $author_position = get_the_author_meta('position');
-?>
 
-<div class="entry__author__overview">
-  <figure class="entry__author__image">
-    <a href="<?php echo $author_url; ?>">
-      <?php echo $avatar_icon; ?>
-    </a>
-  </figure>
-  <div class="entry__author__names">
-    <p class="entry__author__names__name">
-      <a href="<?php echo $author_url; ?>"><?php echo $author_name; ?></a>
-      <span class="entry__author__names__post-count">(記事投稿数: <?php echo $author_posts_count; ?>)</span>
-    </p>
-    <p class="entry__author__names__position"><?php echo $author_position; ?></p>
-  </div>
-</div>
-<div class="entry__author__description">
-  <p class="entry__author__description__p"><?php echo $author_description; ?></p>
-</div>
+  if (! empty($author_description)) { ?>
+    <div class="entry__author__overview">
+      <figure class="entry__author__image">
+        <a href="<?php echo $author_url; ?>">
+          <?php echo $avatar_icon; ?>
+        </a>
+      </figure>
+      <div class="entry__author__names">
+        <p class="entry__author__names__name">
+          <a href="<?php echo $author_url; ?>"><?php echo $author_name; ?></a>
+          <?php /* <span class="entry__author__names__post-count">(記事投稿数: <?php echo $author_posts_count; ?>)</span> */ ?>
+        </p>
+        <p class="entry__author__names__position"><?php echo $author_position; ?></p>
+      </div>
+    </div>
+    <div class="entry__author__description">
+      <p class="entry__author__description__p"><?php echo $author_description; ?></p>
+    </div>
+    <?php
+  }
+
