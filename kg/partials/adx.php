32591f762eca71442db35355a7f2397c87f4d646
Yoshi Ito <thanks2music@gmail.com>
2019-08-17 21:35:10 +0900

    アナリティクス/タグマネージャー/アドセンスなどの部分をコメントアウト

--- kg/partials/adx.php
+++ kg/partials/adx.php
@@ -1,12 +1,13 @@
 <?php global $is_sp, $is_pc ?>
 <?php // Google Adsense ?>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<?php /* TODO : アドセンス作ったら差し替える
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
-<?php if ($is_sp) { ?>
-  <?php // GMO YDN Tag ?>
-  <script src="//cdn.gmossp-sp.jp/ads/loader.js?space_id=g908845" charset="utf-8" language="JavaScript"></script>
-  <script language='javascript'>
-      gmossp_forsp.ad('g908845');
-  </script>
-<?php }
-
+  <?php if ($is_sp) { ?>
+    <?php // GMO YDN Tag ?>
+    <script src="//cdn.gmossp-sp.jp/ads/loader.js?space_id=g908845" charset="utf-8" language="JavaScript"></script>
+    <script language='javascript'>
+        gmossp_forsp.ad('g908845');
+    </script>
+  <?php }
+*/ ?>
--- kg/partials/meta/gtm.php
+++ kg/partials/meta/gtm.php
@@ -1,4 +1,4 @@
-<?php
+<?php /* TODO アナリティクス作ったら差し替える
 if (! empty($_GET['amp'])) {
   if ($_GET['amp'] === '1') { ?>
   <amp-analytics config="https://www.googletagmanager.com/amp.json?id=GTM-M7TH3K3&gtm.url=SOURCE_URL" data-credentials="include"></amp-analytics>
@@ -8,3 +8,4 @@ if (! empty($_GET['amp'])) {
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo get_option('other_options_ga'); ?>"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <?php } ?>
+*/ ?>
