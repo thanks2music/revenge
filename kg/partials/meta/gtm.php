10ac755e3b78b9a3f50ae120686e312d49ccc9da
Yoshi Ito <thanks2music@gmail.com>
2019-08-25 16:45:03 +0900

    Fix GTM id

--- kg/partials/meta/gtm.php
+++ kg/partials/meta/gtm.php
@@ -5,6 +5,6 @@ if (! empty($_GET['amp'])) {
   <?php }
 } else if (get_option('other_options_ga')) { ?>
 <?php // GTM No Script ?>
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WT89942"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo get_option('other_options_ga'); ?>"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <?php } ?>
