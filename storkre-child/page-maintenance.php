57f9bf16d36119ca6964ca2278dd2f4ef5b16b05
Yoshi Ito <thanks2music@gmail.com>
2020-05-29 22:45:48 +0900

    Add maintenance page

---
+++ storkre-child/page-maintenance.php
@@ -0,0 +1,234 @@
+<!doctype html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="robots" content="noindex, nofollow">
+    <title>メンテナンス中のため閲覧出来ません | コラボカフェ</title>
+    <meta name="description" content="説明文">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','<?php echo get_option('other_options_ga'); ?>');</script>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TPJTNFV');</script>
+    <style>
+      html {
+        font-size: 16px;
+        height:100%;
+      }
+
+      @media screen and (max-width: 720px) {
+        html {
+          font-size: 14px;
+        }
+      }
+
+      @media screen and (max-width: 480px) {
+        html {
+          font-size: 12px;
+        }
+      }
+
+      body {
+        font-family:'ヒラギノ角ゴ Pro W3', 'Hiragino Kaku Gothic Pro', 'メイリオ', 'Meiryo', 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif;
+        background-color: #000;
+        margin: 0;
+        text-align: center;
+        height: 100%;
+      }
+
+      p {
+        color: #5d5d5d;
+        margin: 0;
+        line-height: 1.6;
+      }
+
+      .wrapper {
+        background-color: #ff5dab;
+        padding: 20px 0;
+        min-height: 90%;
+      }
+
+      @media screen and (max-width: 720px) {
+        .wrapper {
+          padding: 5px 0;
+        }
+      }
+
+      .main-contents {
+        display: block;
+        width: 80%;
+        max-width: 720px;
+        -webkit-border-radius: 10px;
+        -moz-border-radius: 10px;
+        border-radius: 10px;
+        margin: 20px auto;
+        padding: 4% 2%;
+      }
+
+      .logo {
+        margin: 0 auto 2%;
+      }
+
+      .logo img {
+        width: 50%;
+      }
+
+      @media screen and (max-width: 720px) {
+        .logo img {
+          width: 60%;
+        }
+      }
+
+      .headline {
+        font-size: 1.6rem;
+        color: #fff;
+        margin: 0 auto 3%;
+      }
+
+      .description {
+        font-size: 1rem;
+        color: #fff;
+        margin-bottom: 4%;
+      }
+
+      .twitter-widget {
+        max-width: 520px;
+        margin: auto;
+      }
+
+      .maintenance-period-box {
+        width: 80%;
+        margin: 0 auto 4%;
+        padding: 0px 7px 7px 7px;
+        -webkit-border-radius: 15px;
+        -moz-border-radius: 15px;
+        border-radius: 15px;
+        background-color: #eeead5;
+      }
+
+      @media screen and (max-width: 720px) {
+        .maintenance-period-box {
+          padding: 0px 4px 4px 4px;
+          width: 90%;
+        }
+      }
+
+      @media screen and (max-width: 480px) {
+        .maintenance-period-box {
+          width: 95%;
+        }
+      }
+
+      .maintenance-period-box h3 {
+        margin: 0 auto;
+        padding-top: 1.5%;
+        width: 50%;
+      }
+
+      .maintenance-period-box img {
+        max-width: 100%;
+        height: auto;
+      }
+
+      .maintenance-period-box p {
+        -webkit-border-radius: 10px;
+        -moz-border-radius: 10px;
+        border-radius: 10px;
+        background-color: #fff;
+        color: #5d5d5d;
+        font-size: 1.2rem;
+        padding: 3% 0;
+      }
+
+      @media screen and (max-width: 480px) {
+        .maintenance-period-box p {
+          font-size: 1.1rem;
+        }
+      }
+
+      .homebtn {
+        margin: 4% auto 5%;
+        display: block;
+        width: 20%;
+        padding: 2% 6%;
+        -webkit-border-radius: 15px;
+        -moz-border-radius: 15px;
+        border-radius: 15px;
+        background-color: #61b8e0;
+        text-decoration: none;
+        box-shadow: 0 3px 0 #4ba4cd;
+        position: relative;
+        line-height: 1;
+        color:#FFFFFF;
+      }
+
+      .homebtn:hover {
+        box-shadow: 0 1px 0 #4ba4cd;
+        top: 2px;
+      }
+
+      .homebtn img {
+        width: 100%;
+        border: 0;
+        vertical-align: bottom;
+      }
+
+      .english-box {
+        border-top: 2px dotted #f8f8f1;
+        margin: 0 2%;
+        padding: 4% 0 0 0;
+      }
+
+      .english-box h3 {
+        margin: 0 auto 1%;
+        color: #db7373;
+        font-size: 1.4rem;
+      }
+
+      .footer {
+        background-color: #000;
+        text-align: center;
+        padding: 1% 0;
+      }
+
+      .copyright {
+        font-size:  0.9rem;
+        color: #fff;
+      }
+    </style>
+  </head>
+  <body>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo get_option('other_options_ga'); ?>"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <div class="wrapper">
+      <main class="main-contents">
+        <header>
+          <h1 class="logo"><img src="https://collabo-cafe.net/wp-content/uploads/logo-vertical.png" alt="コラボカフェ"></h1>
+        </header>
+        <div class="contents">
+          <h2 class="headline">ただいま、メンテナンス中のためサイトを閲覧出来ません。</h2>
+          <p class="description">
+            日頃より「コラボカフェ」をご利用いただき、誠にありがとうございます。<br>ただいま「コラボカフェ」はメンテナンス中のため閲覧が出来ません。<br>お客様にはたいへんご不便とご迷惑をおかけいたしまして申し訳ありません。<br>
+            お手数ですが最新情報はTwitterをご覧ください。
+          </p>
+          <div class="twitter-widget">
+            <blockquote class="twitter-tweet"><p lang="ja" dir="ltr">【重要なお知らせ】<br>現在ネットワーク障害によりコラボカフェ公式HPへのアクセスが一時的に難しくなっており、調査を行っております。<br><br>ユーザーの皆様にはご不便をおかけし、申し訳ありません。<br>復旧次第公式ツイッター( <a href="https://twitter.com/collabo_cafe?ref_src=twsrc%5Etfw">@collabo_cafe</a> )にてお知らせいたします。</p>&mdash; コラボカフェ (@collabo_cafe) <a href="https://twitter.com/collabo_cafe/status/1266301353585827840?ref_src=twsrc%5Etfw">May 29, 2020</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+          </div>
+        </div>
+      </main>
+    </div>
+    <footer class="footer">
+      <p class="copyright">
+        <small class="copyright">&copy; Collabo Cafe, Inc.</small>
+      </p>
+    </footer>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+  </body>
+</html>
