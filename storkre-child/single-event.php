9d9ba4c4e12a689f6aeead598dfad823eb3b109d
Yoshi Ito <thanks2music@gmail.com>
2020-09-28 23:48:39 +0900

    Add single-event template

--- storkre-child/single-event.php
+++ storkre-child/single-event.php
@@ -1,4 +1,6 @@
 <?php global $amp_flag;
+  $post_type = get_post_type();
+
   if (! empty($_GET['amp'])) {
     if (is_single() && $_GET['amp'] === '1') {
       $amp_flag = true;
@@ -7,6 +9,8 @@
 
   if ($amp_flag) {
     get_template_part('single-amp');
+  } elseif ($post_type ==='event') {
+    get_template_part('single-event-revenge');
   } else {
     get_template_part('single');
   }
