f9c5aaab852002ad8ee5191afa599a97a94ad925
Yoshi Ito <thanks2music@gmail.com>
2021-07-05 17:49:57 +0900

    FastCGIに変更したら、headerでエラーが出た修正

--- storkre-child/config/cleanup.php
+++ storkre-child/config/cleanup.php
@@ -15,7 +15,7 @@
   // ショートカットURLを表示しない
   remove_action('wp_head', 'wp_shortlink_wp_head');
   // 絵文字関連情報を消す
-  remove_action('wp_head', 'print_emoji_detection_script', 7);
+  // remove_action('wp_head', 'print_emoji_detection_script', 7);
   remove_action('wp_print_styles', 'print_emoji_styles');
   // oEmbed情報を消す
   remove_action('wp_head','rest_output_link_wp_head');
