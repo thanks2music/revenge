650d835701fc943f34b97476efee0cb848b94884
Yoshiyuki Ito <thanks2music@gmail.com>
2024-01-25 17:16:25 +0900

    Add flux ad tag, if flag of custom field is on

--- storkre-child/partials/adx.php
+++ storkre-child/partials/adx.php
@@ -1,17 +1,19 @@
-<?php global $is_sp, $is_pc ?>
+<?php global $is_sp, $is_pc, $post ?>
 <?php // Google Adsense ?>
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
+<?php if (is_single() && is_hide_google_adsense($post->ID)) { ?>
+  <script src="https://flux-cdn.com/client/1000300/collabo-cafe_01514.min.js" async></script>
+<?php } ?>
+
 <?php if ($is_sp && is_web()) { ?>
-  <?php if (! is_app()) { // Geniee Tag for Google Ad Manager ?>
-    <script>
-      window.gnshbrequest = window.gnshbrequest || {cmd:[]};
-        gnshbrequest.cmd.push(function(){
-          gnshbrequest.registerPassback("1511018");
-          gnshbrequest.forceInternalRequest();
-      });
-    </script>
-    <script async src="https://cpt.geniee.jp/hb/v1/213660/461/wrapper.min.js"></script>
-    <script src="https://cpt.geniee.jp/hb/v1/213660/461/instbody.min.js"></script>
-  <?php } ?>
+  <script>
+    window.gnshbrequest = window.gnshbrequest || {cmd:[]};
+      gnshbrequest.cmd.push(function(){
+        gnshbrequest.registerPassback("1511018");
+        gnshbrequest.forceInternalRequest();
+    });
+  </script>
+<script async src="https://cpt.geniee.jp/hb/v1/213660/461/wrapper.min.js"></script>
+<script src="https://cpt.geniee.jp/hb/v1/213660/461/instbody.min.js"></script>
 <?php } ?>
